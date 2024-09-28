500d621819e214e079d5d7fcc630278c50283605
unknown <osafridi00@gmail.com>
2024-09-28 23:35:59 +0600

    absolute Path

--- next-demo/src/app/page.tsx
+++ next-demo/src/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import homeImage from "@/../public/home.jpg";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImage}
+          alt="homeImage"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
