73c52f96d243a3fe1d6a86b13ecc412fe153d98a
unknown <osafridi00@gmail.com>
2024-09-28 16:17:56 +0600

    layout

--- next-demo/src/app/page.tsx
+++ next-demo/src/app/page.tsx
@@ -1,18 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <Link href={"/performance"}>Performance</Link>
-      </div>
-      <div>
-        <Link href={"/scale"}>Scale</Link>
-      </div>
-      <div>
-        <Link href={"/reliability"}>Reliability</Link>
-      </div>
-      Home Page
-    </div>
-  );
+  return <div>Home Page</div>;
 }
