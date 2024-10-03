56c02560278a05eb19a7bcea74c157aa9443f166
omar afridi <osafridi00@gmail.com>
2024-10-03 12:51:35 +0600

    align

--- next-demo/src/components/header.tsx
+++ next-demo/src/components/header.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="z-10 text-white w-full absolute shadow-2xl">
-      <nav className="flex flex-wrap text-center relative justify-between p-8 w-full">
+      <nav className="flex flex-wrap text-center relative justify-between px-8 py-6 w-full">
         <Link className="text-3xl font-bold" href={"/"}>
           Home
         </Link>
-        <div className="text-xl space-x-4">
+        <div className="text-xl space-x-4 self-center">
           <Link href={"/performance"}>Performance</Link>
           <Link href={"/scale"}>Scale</Link>
           <Link href={"/reliability"}>Reliability</Link>
