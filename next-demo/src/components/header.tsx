36d76459e627e831c9d5e208587772a88d1615ff
omar afridi <osafridi00@gmail.com>
2024-10-03 12:41:30 +0600

    header

--- next-demo/src/components/header.tsx
+++ next-demo/src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="z-10 text-white w-full absolute ">
+    <div className="z-10 text-white w-full absolute shadow-2xl">
       <nav className="flex flex-wrap text-center relative justify-between p-8 w-full">
         <Link className="text-3xl font-bold" href={"/"}>
           Home
